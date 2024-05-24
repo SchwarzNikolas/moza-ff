a842ec5c5cb1536a2667b2fc521b35fdbfb2763f
Nikolas <nikolas@a-schwarz.info>
2024-05-24 21:58:11 +0200

    changed usb id

--- hid-simagic.h
+++ hid-simagic.h
@@ -2,8 +2,8 @@
 #ifndef __HID_SIMAGIC_H
 #define __HID_SIMAGIC_H
 
-#define USB_VENDOR_ID_SIMAGIC		0x0483
-#define USB_DEVICE_ID_SIMAGIC_WHEEL 0x0522
+#define USB_VENDOR_ID_SIMAGIC		0x346e
+#define USB_DEVICE_ID_SIMAGIC_WHEEL 0x0006
 
 int hid_pidff_init_simagic(struct hid_device *hdev);
 
